27dbde96ca5850f5c432eae63ceacdb65faf0972
Lukas Morrison <lukascmorrison@gmail.com>
2022-02-21 12:36:13 -0700

    booking confirmation working and tested 3 retries implemented

--- GymBot.py
+++ GymBot.py
@@ -35,7 +35,7 @@ login_url = "https://iac01.ucalgary.ca/CamRecWebBooking/Login.aspx"
 default_url = "https://iac01.ucalgary.ca/CamRecWebBooking/default.aspx"
 
 if __name__ == "__main__":
-    print("GymBot® v0.15")
+    print("GymBot® v0.15.2")
 
     # Define objects
     ser = Service(driverFileName)
