4e199923ce7a9671dc4c254e1fcf05dea06aa2b0
Lukas Morrison <lukascmorrison@gmail.com>
2022-02-27 16:40:42 -0700

    in progress - checkboxes - window reformating - settings icon

---
+++ GymBot_Compile.bat
@@ -0,0 +1,8 @@
+pyinstaller --onefile ^
+--add-binary "GymBot.ico;files" ^
+--add-binary "GymBot_light.png;files" ^
+--add-binary "GymBot_dark.png;files" ^
+--add-binary "GymBot.png;files" ^
+--add-binary "chromedriver.exe;files" ^
+--add-binary "credentials.json;files" ^
+-i GymBot.ico GymBot.py
