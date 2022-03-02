0aadbfcf28271110c8514f9202a0181aab39985d
Lukas Morrison <lukascmorrison@gmail.com>
2022-03-02 14:47:58 -0700

    v0.21 - next day booking support

--- GymBot.py
+++ GymBot.py
@@ -44,7 +44,7 @@ if not os.path.exists(localPath):
     os.makedirs(localPath)
 
 if __name__ == "__main__":
-    print("GymBot® v0.20")
+    print("GymBot® v0.21")
 
     # Define objects
     ser = Service(driverFileName)
