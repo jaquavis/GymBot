ca44582e1de404d9ed0c7511668a51ade97b034d
Lukas Morrison <lukascmorrison@gmail.com>
2022-03-23 17:16:52 -0600

    v1.5

--- GymBot_Compile_Linux.sh
+++
@@ -1,7 +0,0 @@
-pyinstaller --onefile \
---add-binary "GymBot.ico:files" \
---add-binary "GymBot_light.png:files" \
---add-binary "GymBot_dark.png:files" \
---add-binary "GymBot.png:files" \
---add-binary "chromedriver_linux64:files" \
--i GymBot.ico GymBot.py
