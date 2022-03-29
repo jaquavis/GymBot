c8daa0b966c101b6efb3509f98cadca438941c89
Lukas Morrison <lukascmorrison@gmail.com>
2022-03-29 12:03:07 -0600

    v1.5

--- GymBot_Compile.bat
+++ GymBot_Compile.bat
@@ -7,5 +7,3 @@ pyinstaller --onefile ^
 --add-binary "credentials.json;files" ^
 -i GymBot.ico GymBot.py ^
 --noconsole
-
-:: pyinstaller --onefile --add-binary "GymBot.ico:files" --add-binary "GymBot_light.png:files" --add-binary "GymBot_dark.png:files" --add-binary "GymBot.png:files" --add-binary "chromedriver_mac64_m1:files" --add-binary "credentials.json:files" -i GymBot.ico GymBot.py --noconsole
---
+++ GymBot_Compile_Mac.sh
@@ -0,0 +1,1 @@
+pyinstaller --onefile --add-binary "GymBot.ico:files" --add-binary "GymBot_light.png:files" --add-binary "GymBot_dark.png:files" --add-binary "GymBot.png:files" --add-binary "chromedriver_mac64_m1:files" --add-binary "credentials.json:files" -i GymBot.ico GymBot.py --noconsole
